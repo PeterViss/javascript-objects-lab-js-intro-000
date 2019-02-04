f7f78e93458f3d16ce6a74b5a8d97f09dfcb094e
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-04 03:34:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function updateObjectWithKeyAndValue(object1, key, value) {
   // { weight: 150 }
   //object1[ key ] = value;
   //return object1;
-  
+
   // NOT CORRECT
   // { key: 150 }
   //object1.key = value;
-  
+
   let object2 = Object.assign(object1, { [key]: value });
   return object2;
 }
